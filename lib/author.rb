aef7b46e6ecc250a2bb7ef78ad8ca3d47e719d80
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-23 19:54:02 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,52 +1,52 @@
-# def Author
-#   attr_accessor :name
-#   # attr_reader :posts
- 
-#   # @@all = []
-  
-#   def initialize(name)
-#     @name = name
-#     # @@all << self
-#   end
-  
-# #   def self.all
-# #     @@all
-# #   end
-  
-# #   def posts
-# #     Posts.all.select {|post| post.author == self}
-# #   end
-  
-# end
-
-class Author
+def Author
   attr_accessor :name
-
-  @@all = []
-
+  # attr_reader :posts
+ 
+  # @@all = []
+  
   def initialize(name)
     @name = name
-    @@all << self
+    # @@all << self
   end
-
-  def self.all
-    @@all
-  end
-
-  # def add_post(post)
-  #   post.author = self
-  # end
-
-  # def add_post_by_title(title)
-  #   post = Post.new(title)
-  #   post.author = self
-  # end
-
-  # def posts
-  #   Post.all.select {|post| post.author == self}
-  # end
-
-  # def self.post_count
-  #   Post.all.count
-  # end
+  
+#   def self.all
+#     @@all
+#   end
+  
+#   def posts
+#     Posts.all.select {|post| post.author == self}
+#   end
+  
 end
+
+# class Author
+#   attr_accessor :name
+
+#   @@all = []
+
+#   def initialize(name)
+#     @name = name
+#     @@all << self
+#   end
+
+#   def self.all
+#     @@all
+#   end
+
+#   # def add_post(post)
+#   #   post.author = self
+#   # end
+
+#   # def add_post_by_title(title)
+#   #   post = Post.new(title)
+#   #   post.author = self
+#   # end
+
+#   # def posts
+#   #   Post.all.select {|post| post.author == self}
+#   # end
+
+#   # def self.post_count
+#   #   Post.all.count
+#   # end
+# end
