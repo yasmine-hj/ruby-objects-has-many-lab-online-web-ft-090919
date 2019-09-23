f5cc721d4f481d38eb1c36e6461a69db2a196880
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-23 19:52:40 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,20 +1,20 @@
-def Author
-  attr_accessor :name
-  # attr_reader :posts
+# def Author
+#   attr_accessor :name
+#   # attr_reader :posts
  
-  # @@all = []
+#   # @@all = []
   
-  def initialize(name)
-    @name = name
-    # @@all << self
-  end
-  
-#   def self.all
-#     @@all
+#   def initialize(name)
+#     @name = name
+#     # @@all << self
 #   end
   
-#   def posts
-#     Posts.all.select {|post| post.author == self}
-#   end
+# #   def self.all
+# #     @@all
+# #   end
   
-end
+# #   def posts
+# #     Posts.all.select {|post| post.author == self}
+# #   end
+  
+# end
