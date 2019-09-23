e07687883bbe2f500ac0f395611dc081ff0652fe
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-23 20:12:13 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,38 @@
+class Author
+  attr_accessor :name
+  attr_reader :posts
+  @@all = []
+  @@post_count = 0
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def add_post(post)
+     @@post_count += 1
+    post.author = self
+  end
+  
+  def add_post_by_title (title)
+     @@post_count += 2
+    post = Post.new(title)
+    post.author = self
+  end
+  
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+  
+  def self.post_count
+    @@post_count
+  end
+end
+
+
+
+
---
+++ lib/post.rb
@@ -0,0 +1,16 @@
+class Post
+  attr_accessor :author, :title
+  @@all = []
+  def initialize (title)
+    @title = title
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def author_name
+  self.author.name if author
+  end
+end
