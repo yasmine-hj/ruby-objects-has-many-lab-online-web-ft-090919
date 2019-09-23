375fc7367378d0d1b16597f582f130c2960b692a
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-23 20:05:13 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -3,6 +3,7 @@ class Author
   attr_reader :posts
  
   @@all = []
+  @@post_count = 0
   
   def initialize(name)
     @name = name
@@ -15,45 +16,24 @@ class Author
   
   def add_post(post)
     post.author = self
+    @@post_count += 1
   end
   
   def add_post_by_title (title)
     post = Post.new(title)
     post.author = self
+    @@post_count += 1
   end
   
   def posts
     Post.all.select {|post| post.author == self}
   end
   
+  
+  
 end
 
-# class Author
-#   attr_accessor :name
 
-#   @@all = []
-
-#   def initialize(name)
-#     @name = name
-#     @@all << self
-#   end
-
-#   def self.all
-#     @@all
-#   end
-
-#   # def add_post(post)
-#   #   post.author = self
-#   # end
-
-#   # def add_post_by_title(title)
-#   #   post = Post.new(title)
-#   #   post.author = self
-#   # end
-
-#   # def posts
-#   #   Post.all.select {|post| post.author == self}
-#   # end
 
 #   # def self.post_count
 #   #   Post.all.count
