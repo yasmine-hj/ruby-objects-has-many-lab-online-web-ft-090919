f67d956bbdeb37bfcf917bdb90cdf5f4bc279e82
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-23 19:53:30 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -33,20 +33,20 @@ class Author
     @@all
   end
 
-  def add_post(post)
-    post.author = self
-  end
+  # def add_post(post)
+  #   post.author = self
+  # end
 
-  def add_post_by_title(title)
-    post = Post.new(title)
-    post.author = self
-  end
+  # def add_post_by_title(title)
+  #   post = Post.new(title)
+  #   post.author = self
+  # end
 
-  def posts
-    Post.all.select {|post| post.author == self}
-  end
+  # def posts
+  #   Post.all.select {|post| post.author == self}
+  # end
 
-  def self.post_count
-    Post.all.count
-  end
+  # def self.post_count
+  #   Post.all.count
+  # end
 end
